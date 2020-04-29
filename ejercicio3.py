40dbd019b164d4d0017c9fe6a695502da4162948
widcatd <32911694+widcatd@users.noreply.github.com>
2020-04-29 14:22:25 -0500

    Delete ejercicio3.py

--- ejercicio3.py
+++
@@ -1,6 +0,0 @@
-import cv2 as cv2
-image = cv2.imread('data/thresh3.png')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow('Original image',image)
-cv2.imshow('Gray image', gray)
-cv2.waitKey(0)
