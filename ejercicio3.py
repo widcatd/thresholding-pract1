b943a32c2350fa73aca088c54bdd05096115b97a
widcatd <32911694+widcatd@users.noreply.github.com>
2020-04-29 14:31:30 -0500

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+image = cv2.imread('data/thresh3.png')
+cv2.imshow('Gray image', image)
+cv2.waitKey()
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+a = np.asarray(gray)
+datos=np.zeros(256)
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        datos[a[i][j]]=datos[a[i][j]]+1
+x=[]
+for i in range(256):
+    x.append(i)
+#plt.plot(x,datos)
+#plt.show()
+limi=192 #limite inicial
+limf=207 #limite final
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        if a[i][j]>=limi and a[i][j]<=limf:
+            a[i][j]=0
+        else:
+            a[i][j]=255
+im_fin = np.uint8(a)
+cv2.imshow('image', im_fin)
+cv2.waitKey()
+cv2.imwrite('./out/e2out_thresh3.png',im_fin)
