2094245eadd5025b751398260bba2d3852399a6b
César Huisa <darkheart.x1@outlook.com>
2020-06-23 11:59:41 -0500

    revision 13hgd

--- op_geom/ejercicio2.py
+++ op_geom/ejercicio2.py
@@ -3,12 +3,31 @@ import os
 import numpy as np
 import math
 
+def ces_affine(img,matrix,tam):
+    A=matrix[0:2,0:2]
+    B=matrix[0:2,2:3]
+    out=np.zeros(shape=tam,dtype=np.uint8)
+    for f in range(len(img[0][0])):
+        for j in range(tam[0]):
+            for k in range(tam[1]):
+                tmp=(np.dot(A,np.array([[j],[k]])))+B
+                x=int(tmp[0][0])
+                y=int(tmp[1][0])
+                if(x<0 or x>=img.shape[0]):
+                    out[j][k][f]=0
+                elif(y<0 or y>=img.shape[1]):
+                    out[j][k][f]=0
+                else:
+                    out[j][k][f]=img[x][y][f]
+    return (out)
+
+
 def op_rotar(img,angle):
     if img is not None:
         filas, cols = img.shape[0], img.shape[1]
         M = cv2.getRotationMatrix2D((int(filas/2), int(cols/2)), angle, 1)
         #M = np.float32([[math.cos(angle),math.sin(angle),((1-math.cos(angle))*2-math.sin(angle)*2)],[-math.sin(angle),math.cos(angle),(math.sin(angle)*2+(1-math.sin(angle)*2))]])
-        out=cv2.warpAffine(img,M,(cols,filas))
+        out=ces_affine(img,M,(cols,filas,3))
         return(out)
     else:
         return(None)
@@ -17,7 +36,7 @@ def op_escala(img,alto,ancho):
     if img is not None:
         filas, cols = img.shape[0], img.shape[1]
         M = np.float32([[ancho,0,0],[0,alto,0]])
-        out=cv2.warpAffine(img,M,(ancho,alto))
+        out=ces_affine(img,M,(ancho,alto,3))
         return(out)
     else:
         return(None)
@@ -26,7 +45,7 @@ def op_trasl(img,tx,ty):
     if img is not None:
         filas, cols = img.shape[0], img.shape[1]
         M = np.float32([[1,0,tx],[0,1,ty]])
-        out=cv2.warpAffine(img,M,(cols,filas))
+        out=ces_affine(img,M,(cols,filas,3))
         return(out)
     else:
         return(None)
@@ -35,11 +54,10 @@ def op_shear(img,shx,shy):
     if img is not None:
         filas, cols = img.shape[0], img.shape[1]
         M = np.float32([[1,shy,0],[shx,1,0]])
-        out=cv2.warpAffine(img,M,(filas,cols))
+        out=ces_affine(img,M,(filas,cols,3))
         return(out)
     else:
         return(None)
-
 
 
 def run_rot(folder,angle):
@@ -48,7 +66,7 @@ def run_rot(folder,angle):
         img = cv2.imread(os.path.join(folder,filename))
         out=op_rotar(img,angle)
         if out is not None:
-            cv2.imwrite('./out/out1_rot'+str(angle)+'_'+filename,out)
+            cv2.imwrite('./out/ceout2_rot'+str(angle)+'_'+filename,out)
             print("Imagen Transformada")
 
 def run_escal(folder,a,an):
@@ -57,7 +75,7 @@ def run_escal(folder,a,an):
         img = cv2.imread(os.path.join(folder,filename))
         out=op_escala(img,a,an)
         if out is not None:
-            cv2.imwrite('./out/out1_esc'+str(a)+'-'+str(an)+'_'+filename,out)
+            cv2.imwrite('./out/ceout2_esc'+str(a)+'-'+str(an)+'_'+filename,out)
             print("Imagen Transformada")
 
 def run_trasl(folder,tx,ty):
@@ -66,7 +84,7 @@ def run_trasl(folder,tx,ty):
         img = cv2.imread(os.path.join(folder,filename))
         out=op_trasl(img,tx,ty)
         if out is not None:
-            cv2.imwrite('./out/out1_tras'+str(tx)+'-'+str(ty)+'_'+filename,out)
+            cv2.imwrite('./out/ceout2_tras'+str(tx)+'-'+str(ty)+'_'+filename,out)
             print("Imagen Transformada")
 
 def run_shear(folder,shx,shy):
@@ -75,7 +93,7 @@ def run_shear(folder,shx,shy):
         img = cv2.imread(os.path.join(folder,filename))
         out=op_shear(img,shx,shy)
         if out is not None:
-            cv2.imwrite('./out/out1_shear'+str(shx)+'-'+str(shy)+'_'+filename,out)
+            cv2.imwrite('./out/ceout2_shear'+str(shx)+'-'+str(shy)+'_'+filename,out)
             print("Imagen Transformada")
 
 if __name__ == "__main__":
