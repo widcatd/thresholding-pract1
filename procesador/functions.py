5b98bf4d184a4a0189877fe176785f6adfc90f67
widcatd <32911694+widcatd@users.noreply.github.com>
2020-07-17 08:06:53 -0500

    Update functions.py

--- procesador/functions.py
+++ procesador/functions.py
@@ -407,3 +407,56 @@ def op_shear(imname,shx,shy):
         return(out)
     else:
         return(None)
+def thresold_adaptativo(direccion,w_size,cf):#segundo parametro debe ser impar mayor que 1
+    img=cv2.imread(direccion)
+    if img is not None:
+        #mostramos las imagenes originales
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('Imagen inicial 1',img)
+        cv2.waitKey()
+        #ajustamos el tamaño de la imagen 2 a la de la imagen 1
+        #creamos una imagen en negro
+        out=np.zeros(shape=img.shape,dtype=np.uint8)
+        #matriz_umbral=np.zeros(shape=img.shape,dtype=np.uint8)
+        submatrix=[]
+        parametro=int(w_size/2)
+        valores=[0,0,0,0]
+        tam=[0,0]
+        suma=0
+        #aplicamos la suma en los 3 canales
+        for i in range(img.shape[0]):
+            for j in range(img.shape[1]):
+                #obtenemos las coordenadas para generar la submatriz
+                if i-parametro>=0:
+                    valores[0]=i-parametro
+                else:
+                    valores[0]=0
+                if i+parametro<img.shape[0]:
+                    valores[1]=i+parametro+1
+                else:
+                    valores[1]=img.shape[0]
+                if j-parametro>=0:
+                    valores[2]=j-parametro
+                else:
+                    valores[2]=0
+                if j+parametro<img.shape[1]:
+                    valores[3]=j+parametro+1
+                else:
+                    valores[3]=img.shape[1]
+                #generamos la submatriz
+                submatrix=np.zeros(shape=[valores[1]-valores[0]-1,valores[3]-valores[2]-1])
+                submatrix=img[valores[0]:valores[1]:1,valores[2]:valores[3]:1].copy()
+                #sacamos el promedio
+                suma=int(np.sum(submatrix)/(submatrix.shape[0]*submatrix.shape[1]))
+                suma=suma-(cf) #restamos con el valor statico
+                #aplicamos thresholding
+                if img[i][j]>=suma:
+                    out[i][j]=255
+                else:
+                    out[i][j]=0 
+        #cv2.imshow('Imagen final',out)
+        #cv2.waitKey()
+        return(out)
+        #guardamos la imagen generada
+    else:
+        return(None)
